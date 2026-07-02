cfa987e48b2ff26d8e93152121b5609e9420231a
ha <hadyaamin712@gmail.com>
2026-07-02 14:59:30 +0430

    improve user experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "11887d043t216f31aoc82b0e0d11614d";
   let context =
-    "You are a romantic Poem expert and love to write short poem, your mission is to generate a 4 line poem in basic HTML, make sure to follow the user instruction.";
+    "You are a romantic Poem expert and love to write short poem, your mission is to generate a 4 line poem in basic HTML, make sure to follow the user instruction. don't write html write jsut the poem";
   let prompt = `user instruction: Generate a French poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.style.display = "block";
+  poemElement.innerHTML = `Generating a French poem about ${instructionsInput.value}`;
   axios.get(apiUrl).then(displayPoem);
 }
 
